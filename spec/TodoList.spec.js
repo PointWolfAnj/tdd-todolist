1608b0ed839e17ea40c206ffcec44827289a874f
PointWolfAnj <anojan31@gmail.com>
2022-01-11 16:59:38 +0000

    done

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -7,7 +7,7 @@ describe("TodoList", () => {
     todoList = new TodoList()
   })
   it("creates a todo item", () => {
-    // set up
+    // set up 
     const expected = {
       id: 1,
       text: 'Take the pizza out of the packaging',
@@ -20,25 +20,24 @@ describe("TodoList", () => {
   })
   it("Get all todo items", () => {
     // set up
-    const one = {
+    const expected = [{
       id: 1,
-      text: 'Place in the microwave',
+      text: 'Take the pizza out of the packaging',
       status: 'incomplete'
-    }
-    const two = {
+    },
+    {
       id: 2,
       text: 'Heat up the pizza for 5 minutes',
       status: 'incomplete'
-    }
+    }]
     // execute
-    todoList.create('Place in the microwave')
+    todoList.create('Take the pizza out of the packaging')
     todoList.create('Heat up the pizza for 5 minutes')
-    const expected = [one, two]
     const result = todoList.getAllTodo()
     // verify
     expect(result).toEqual(expected)
   })
-  it("Set a todo completed by its ID", () => {
+  it('Set a todo completed by its ID', () => {
     // set up
     const expected = {
       id: 1,
@@ -53,28 +52,75 @@ describe("TodoList", () => {
   })
   it('Get all todo items', () => {
     // set up
-    const one = {
-      id: 1,
-      text: 'Eat your pizza',
-      status: 'complete'
-    }
-    const two = {
+    const expected = [{
       id: 2,
       text: 'Heat up the pizza for 5 minutes',
       status: 'incomplete'
-    }
-    const three = {
-      id: 2,
+    },
+    {
+      id: 3,
       text: 'Clean your plate',
       status: 'incomplete'
-    }
+    }]
     // execute
     todoList.create('Eat your pizza')
     todoList.create('Heat up the pizza for 5 minutes')
     todoList.create('Clean your plate')
-    const expected = [two, three]
     todoList.setToDoComplete(1)
     const result = todoList.getIncomplete()
+    // verify
+    expect(result).toEqual(expected)
+  })
+  it('Get all todo items', () => {
+    // set up
+    const expected = [{
+      id: 1,
+      text: 'Take the pizza out of the microwave',
+      status: 'complete'
+    }]
+    // execute
+    todoList.create('Take the pizza out of the microwave')
+    todoList.create('Heat up the pizza for 5 minutes')
+    todoList.create('Clean your plate')
+    todoList.setToDoComplete(1)
+    const result = todoList.getcomplete()
+    // verify
+    expect(result).toEqual(expected)
+  })
+  it('Search and return a todo item by its ID', () => {
+    // set up
+    const expected = {
+      id: 3,
+      text: 'Clean your plate',
+      status: 'incomplete'
+    }
+    // execute
+    todoList.create('Take the pizza out of the microwave')
+    todoList.create('Heat up the pizza for 5 minutes')
+    todoList.create('Clean your plate')
+    const result = todoList.searchItem(3)
+    // verify
+    expect(result).toEqual(expected)
+  })
+  it('return a message saying it does not exist', () => {
+    // set up
+    const expected = 'N/A'
+
+    // execute
+    todoList.create('Take the pizza out of the microwave')
+    todoList.create('Heat up the pizza for 5 minutes')
+    todoList.create('Clean your plate')
+    const result = todoList.searchItem(4)
+    // verify
+    expect(result).toEqual(expected)
+  })
+  it('Remove a todo item by its ID', () => {
+    // set up
+    const expected = [
+    ]
+    // execute
+    todoList.create('Take the pizza out of the packaging')
+    const result = todoList.removeItem(1)
     // verify
     expect(result).toEqual(expected)
   })
